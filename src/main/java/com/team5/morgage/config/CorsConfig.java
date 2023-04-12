9449f178031b5a26c0424c854e19e453e9c84998
[Valdemaras] <[valdemarlamm@gmail.com]>
2023-04-12 10:10:21 +0300

    cors edit.

--- src/main/java/com/team5/morgage/config/CorsConfig.java
+++ src/main/java/com/team5/morgage/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:4200","https://mortgage-loan.onrender.com/")
                 .allowedMethods("GET", "POST");
     }
 }
