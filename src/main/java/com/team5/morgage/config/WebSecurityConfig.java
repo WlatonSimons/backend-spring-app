8164e1455b64958f4200d360d666ffc6189ec53c
[Valdemaras] <[valdemarlamm@gmail.com]>
2023-04-12 13:27:01 +0300

    Security for docker update.

--- src/main/java/com/team5/morgage/config/WebSecurityConfig.java
+++ src/main/java/com/team5/morgage/config/WebSecurityConfig.java
@@ -10,23 +10,25 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class WebSecurityConfig {
 
-
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-
         http.csrf().disable()
                 .authorizeRequests()
-                .requestMatchers("/").permitAll()
+                .requestMatchers("/**").permitAll()
                 .requestMatchers("/h2-console/**").permitAll()
                 .and()
-                .cors();
+                .cors()
+                .and()
+                .authorizeRequests()
+                .requestMatchers("/**").hasIpAddress("3.75.158.163")
+                .requestMatchers("/**").hasIpAddress("3.125.183.140")
+                .requestMatchers("/**").hasIpAddress("35.157.117.28");
         return http.build();
     }
 }
