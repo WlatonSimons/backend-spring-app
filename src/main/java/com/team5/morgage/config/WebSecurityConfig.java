6236885ea8107f7ed869b4dde5b5925cce5f0d31
[Valdemaras] <[valdemarlamm@gmail.com]>
2023-04-12 16:01:17 +0300

    Security for docker update.

---
+++ src/main/java/com/team5/morgage/config/WebSecurityConfig.java
@@ -0,0 +1,28 @@
+package com.team5.morgage.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+public class WebSecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.csrf().disable()
+                .authorizeRequests()
+                .requestMatchers("/**").permitAll()
+                .and().cors()
+                .and().authorizeRequests();
+        return http.build();
+    }
+}
