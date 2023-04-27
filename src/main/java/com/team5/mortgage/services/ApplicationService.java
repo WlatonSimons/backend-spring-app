c88f7793eb4ab8e06e139e4fae79951ad57a7fb8
nikita <nikivavlt@gmail.com>
2023-04-27 09:55:03 +0300

    Feat: Create application search bar logic

--- src/main/java/com/team5/mortgage/services/ApplicationService.java
+++ src/main/java/com/team5/mortgage/services/ApplicationService.java
@@ -7,11 +7,14 @@ import com.team5.mortgage.validators.ApplicationValidator;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.util.Streamable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ApplicationService {
@@ -48,5 +51,37 @@ public class ApplicationService {
         } else {
             throw new CustomException("New status is empty or null");
         }
+    }
+
+    public ResponseEntity<List<Application>> getApplications(Long id, String term) {
+        List <Application> applications = new ArrayList<>();
+
+        if (id != null) {
+            String normalizedId = id.toString();
+
+            getAllApplications()
+                    .stream()
+                    .filter(application -> application.getId().toString().contains(normalizedId))
+                    .forEach(applications :: add);
+        }
+        if (term != null) {
+            String normalizedTerm = term.toLowerCase();
+
+            getAllApplications()
+                    .stream()
+                    .filter(application -> ( application.getFirstName().toLowerCase().contains(normalizedTerm) ) ||
+                            application.getLastName().toLowerCase().contains(normalizedTerm))
+                    .forEach(applications::add);
+        }
+        if (id == null && term == null) {
+            applications.addAll(getAllApplications());
+        }
+
+        if (applications.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(applications, HttpStatus.OK);
+
     }
 }
